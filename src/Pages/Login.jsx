a1b4bf60ed4416f641d4a6f55d54145a6b707ef9
rishabhjain2601 <rishabhjain2dtu@gmail.com>
2023-07-08 22:42:36 +0530

    update

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -20,6 +20,8 @@ const Login = () => {
             </div>
             <div className="cards">
                 <div className="card1"></div>
+                <div className="card2"></div>
+                <div className="card3"></div>
             </div>
         </div>
     )
